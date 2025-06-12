c5d36cbeec2c6e6cd75c3e071da16f0056ebe13b
Astavicious <65663903+Astavicious@users.noreply.github.com>
2025-06-12 22:27:51 +0200

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.py
@@ -0,0 +1,18 @@
+# Last updated: 6/12/2025, 8:27:50 PM
+class Solution(object):
+    def runningSum(self, nums):
+        for i in range(len(nums)):
+            if i>0:
+                nums[i]=nums[i-1]+nums[i]
+        return nums
+            
+
+
+
+
+
+
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
