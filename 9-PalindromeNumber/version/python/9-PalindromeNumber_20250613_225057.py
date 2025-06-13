0eba69eacfb45a846726398abc7a25373c9ef3b6
Astavicious <65663903+Astavicious@users.noreply.github.com>
2025-06-13 22:50:58 +0200

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/version/python/9-PalindromeNumber_20250613_225057.py
@@ -0,0 +1,15 @@
+# Last updated: 6/13/2025, 10:50:57 PM
+class Solution(object):
+    def isPalindrome(self, x):
+        ori=x
+        y=0
+        while(x>0):           
+            y=y*10+x%10
+            x=x//10
+        if ori==y: return True
+        return False
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
