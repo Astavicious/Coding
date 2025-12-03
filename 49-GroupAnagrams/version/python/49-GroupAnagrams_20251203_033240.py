5d9ecb06f549bd52484915f5ae3352a7939de82e
Astavicious <65663903+Astavicious@users.noreply.github.com>
2025-12-03 03:34:39 +0100

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/python/49-GroupAnagrams_20251203_033240.py
@@ -0,0 +1,21 @@
+# Last updated: 12/3/2025, 3:32:40 AM
+1
+2
+3class Solution(object):
+4    def groupAnagrams(self, strs):
+5        out= defaultdict(list)
+6        
+7        for i in strs:
+8            count=[0]*26
+9            for j in i:
+10                x=ord(j)-ord('a')
+11                count[x]+=1
+12            key=tuple(count)
+13            out[key].append(i)
+14        return out.values()
+15
+16        """
+17        :type strs: List[str]
+18        :rtype: List[List[str]]
+19        """
+20        
