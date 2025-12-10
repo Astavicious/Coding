59c93979d29323e245805e90d54e7ba67ceed046
Astavicious <65663903+Astavicious@users.noreply.github.com>
2025-12-10 01:28:37 +0100

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/version/python/121-BestTimetoBuyandSellStock_20251210_012632.py
@@ -0,0 +1,22 @@
+# Last updated: 12/10/2025, 1:26:32 AM
+1class Solution(object):
+2    def maxProfit(self, prices):
+3        i=0
+4        r=len(prices)
+5        profit=0
+6        mini=999999
+7        while i<r:           
+8            profit=max(profit,prices[i]-mini)
+9            mini=min(mini,prices[i])
+10            i+=1
+11            
+12            
+13           
+14        return profit
+15
+16            
+17        """
+18        :type prices: List[int]
+19        :rtype: int
+20        """
+21        
