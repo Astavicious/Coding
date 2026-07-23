870c27ad9ed60e1b4ffe0068382216bad3dd9bfa
Astavicious <65663903+Astavicious@users.noreply.github.com>
2026-07-23 15:08:45 +0300

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.py
@@ -0,0 +1,40 @@
+# Last updated: 23/07/2026, 15:08:44
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy=ListNode()
+        current=dummy
+        carry=0
+        while l1 or l2 or carry:
+            #to get the digits
+            dl1=0
+            dl2=0
+            if l1:
+                dl1=l1.val
+            if l2:
+                dl2=l2.val
+            total=dl1+dl2+carry
+            #summation and carry for the next node
+            carry=total//10
+            ans=total %10
+            #now moving the current node forward and adding the digit
+            current.next=ListNode(ans)
+            current=current.next
+            #now moving l1 and l2 forward
+            if l1:l1=l1.next
+            if l2:l2=l2.next
+        return dummy.next
+
+
+
+        
+        
