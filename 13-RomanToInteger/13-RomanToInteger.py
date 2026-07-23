ede3ebffbad59afaa2e60ca3b64d732c6ded99c3
Astavicious <65663903+Astavicious@users.noreply.github.com>
2026-07-23 15:08:37 +0300

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,26 @@
+# Last updated: 23/07/2026, 15:08:36
+class Solution(object):
+    def romanToInt(self, s):
+        d={"I":1,  
+          "V":5,
+          "X":10,
+          "L":50,
+          "C":100,
+          "D":500,
+          "M":1000
+          }
+        sum=0
+        for i in range(len(s)-1):
+            if d[s[i]]<d[s[i+1]]:
+                sum-=d[s[i]]
+            else:
+                sum+=d[s[i]]
+        return sum+d[s[-1]]
+        
+    
+ 
+        """
+        :type s: str
+        :rtype: int
+        """
+        
